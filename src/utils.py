dfe8f1456a277f1e9aedd36facd2bc4c06238b74
elhy1999 <ernestliu64@gmail.com>
2024-02-12 14:51:25 +0800

    Make data and model paths global variables under the utils package

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
 import os
 
 REPO_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).replace("\\", "/")
+DATA_PATH = os.path.join(REPO_PATH, "data")
+MODELS_PATH = os.path.join(REPO_PATH, "models")
+SRC_PATH = os.path.join(REPO_PATH, "src")
+NOTEBOOKS_PATH = os.path.join(REPO_PATH, "notebooks")
